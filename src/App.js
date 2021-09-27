a640a260008db94698843d8078ad2d51dc5feb13
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-09-27 19:19:48 +0100

    Practice & Component are done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Component } from "react";
+import Movies from "./components/movies";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <h1>Hello</h1>
+        <Movies />
       </main>
     );
   }
